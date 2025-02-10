5f18dc959d3953578e48224c2527145f127d5b24
Nicholas Gabriel Gonzales <nina.gonzales.up@phinmaed.com>
2025-02-10 23:11:19 +0800

    Initial commit

---
+++ scripts/chart.js
@@ -0,0 +1,25 @@
+const xValues = ["Approved", "Pending", "Rejected"];
+const yValues = [50, 50, 0];
+const barColors = [
+    "#4CAF50",
+    "#FFC107",
+    "#F44336"
+];
+
+new Chart("myChart", {
+    type: "doughnut",
+    data: {
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues,
+            borderWidth: 0
+        }]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+            }
+        }
+    }
+});
